f6c15e8cf2b7a2d8774795fc3fcb88c7bad34433
777388 <96343159+777388@users.noreply.github.com>
2023-02-16 14:49:00 -0800

    Create rev-aes-d.py

---
+++ rev-aes-d.py
@@ -0,0 +1,26 @@
+from Crypto.Cipher import AES
+
+# define the encryption key and block size (must match the ones used for encryption)
+key = b'your_key_here'
+block_size = 16
+
+# read the contents of the reversed encrypted file
+with open('encrypted_file.txt', 'rb') as f:
+    reversed_encrypted_data = f.read()
+
+# reverse the encrypted data to restore the original order
+encrypted_data = reversed_encrypted_data[::-1]
+
+# create a new AES cipher object with the key and mode of operation
+cipher = AES.new(key, AES.MODE_ECB)
+
+# decrypt the data using the cipher
+decrypted_data = cipher.decrypt(encrypted_data)
+
+# remove the padding from the decrypted data
+padding = decrypted_data[-1]
+decrypted_data = decrypted_data[:-padding]
+
+# write the decrypted data to a new file
+with open('decrypted_file.txt', 'wb') as f:
+    f.write(decrypted_data)
